b72216905c6ede0eeadab5e3db65438c0a03a901
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-04-12 22:33:55 +0300

    Updated run_quiz_bot.py

--- run_quiz_bot.py
+++ run_quiz_bot.py
@@ -1,5 +1,6 @@
+import json
 import re
-from pprint import pprint
+
 from pathlib import Path
 
 
@@ -18,9 +19,18 @@ def parse_questions(text: str) -> list[str]:
 def parse_question_notes(question: str) -> dict[str:str]:
     question_notes = {}
 
-    for notes in question.split('\n\n'):
-        notes = notes.split(':\n', maxsplit=1)
-        question_notes[notes[0]] = notes[1].strip()
+    for notes in question.strip().split('\n\n'):
+        if notes:
+            try:
+                notes = notes.split(':\n', maxsplit=1)
+                question_notes[notes[0]] = notes[1].strip()
+
+            except IndexError as err:
+                print(f'ERROR: {quiz_file} {err}')
+                question_notes = {f'ERROR {err}': (question, notes)}
+                quiz_parser_errors[quiz_file.name] = question_notes
+
+                return question_notes
 
     return question_notes
 
@@ -33,12 +43,19 @@ def sterilize_questions(questions: list) -> dict[int:dict[str:str]]:
 
 
 if __name__ == '__main__':
-    quiz_folder_path = Path('./quiz-questions/')
+    quiz_folder_path = Path('quiz-questions')
+    parser_folder_path = quiz_folder_path / 'quizzes_parser'
+    parser_folder_path.mkdir(exist_ok=True)
 
     quizzes = {}
+    quiz_parser_errors = {}
 
     for quiz_file in quiz_folder_path.iterdir():
+        if quiz_file.is_file():
+            quizzes[quiz_file] = parse_questions(quiz_file.read_text(encoding='KOI8-R'))
 
-        quizzes[quiz_file] = parse_questions(quiz_file.read_text(encoding='KOI8-R'))
+    with open(parser_folder_path / 'quizzes.json', 'w', encoding='UTF-8') as file:
+        json.dump(quiz_parser_errors, file, ensure_ascii=False, indent=4)
 
-    pprint(quizzes)
+    with open(parser_folder_path / 'errors.json', 'w', encoding='UTF-8') as file:
+        json.dump(quiz_parser_errors, file, ensure_ascii=False, indent=4)
