d38135ff51d2bda1e9d9e6db4b9cf3e670291d7d
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-04-26 21:44:18 +0300

    Optimized redis_db.py

--- redis_db.py
+++ redis_db.py
@@ -5,6 +5,18 @@ logger = logging.getLogger(__file__)
 
 from environs import Env
 
+def connect(host: str, port: int, password: str) -> redis.StrictRedis:
+    database = redis.StrictRedis(
+        host=host,
+        port=port,
+        password=password,
+        charset='utf-8',
+        decode_responses=True
+    )
+
+    return database
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
     logger.setLevel(logging.DEBUG)
@@ -12,16 +24,15 @@ if __name__ == '__main__':
     env = Env()
     env.read_env()
     db_host = env.str('REDIS_HOST')
-    db_port = env.str('REDIS_PORT')
+    db_port = env.int('REDIS_PORT')
     db_password = env.str('REDIS_PASSWORD')
 
 
-    db = redis.StrictRedis(
+    db = connect(
         host=db_host,
         port=db_port,
         password=db_password,
-        charset='utf-8',
-        decode_responses=True
     )
 
     db.set('test key', 'test_value', 20)
+
